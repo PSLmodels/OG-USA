7db8e15cc96c3004a1b993daed3d10eb71ec7841
jdebacker <jason.debacker@gmail.com>
2021-03-02 12:31:51 -0500

    update tests for new demog file

--- ogusa_calibrate/tests/test_demographics.py
+++ ogusa_calibrate/tests/test_demographics.py
@@ -1,6 +1,5 @@
 import numpy as np
-import pytest
-from ogusa import demographics
+from ogusa_calibrate import demographics
 
 
 def test_get_pop_objs():
@@ -11,13 +10,12 @@ def test_get_pop_objs():
     E = 20
     S = 80
     T = int(round(4.0 * S))
-    start_year = 2018
+    start_year = 2019
 
-    (omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector, imm_rates,
-        omega_S_preTP) = demographics.get_pop_objs(E, S, T, 1, 100,
-                                                   start_year, False)
+    pop_dict = demographics.get_pop_objs(
+        E, S, T, 1, 100, start_year, False)
 
-    assert (np.allclose(omega_SS, omega[-1, :]))
+    assert (np.allclose(pop_dict['omega_SS'], pop_dict['omega'][-1, :]))
 
 
 def test_pop_smooth():
@@ -27,14 +25,15 @@ def test_pop_smooth():
     E = 20
     S = 80
     T = int(round(4.0 * S))
-    start_year = 2018
+    start_year = 2019
 
-    (omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector, imm_rates,
-        omega_S_preTP) = demographics.get_pop_objs(E, S, T, 1, 100,
-                                                   start_year, False)
+    pop_dict = demographics.get_pop_objs(
+        E, S, T, 1, 100, start_year, False)
 
-    assert (np.any(np.absolute(omega[:-1, :] - omega[1:, :]) < 0.0001))
-    assert (np.any(np.absolute(g_n_vector[:-1] - g_n_vector[1:]) < 0.0001))
+    assert (np.any(np.absolute(pop_dict['omega'][:-1, :] -
+                               pop_dict['omega'][1:, :]) < 0.0001))
+    assert (np.any(np.absolute(pop_dict['g_n'][:-1] -
+                               pop_dict['g_n'][1:]) < 0.0001))
 
 
 def test_imm_smooth():
@@ -44,14 +43,13 @@ def test_imm_smooth():
     E = 20
     S = 80
     T = int(round(4.0 * S))
-    start_year = 2018
+    start_year = 2019
 
-    (omega, g_n_ss, omega_SS, surv_rate, rho, g_n_vector, imm_rates,
-        omega_S_preTP) = demographics.get_pop_objs(E, S, T, 1, 100,
-                                                   start_year, False)
+    pop_dict = demographics.get_pop_objs(
+        E, S, T, 1, 100, start_year, False)
 
-    assert (np.any(np.absolute(imm_rates[:-1, :] - imm_rates[1:, :]) <
-                   0.0001))
+    assert (np.any(np.absolute(pop_dict['imm_rates'][:-1, :] -
+                               pop_dict['imm_rates'][1:, :]) < 0.0001))
 
 
 def test_get_fert():
@@ -81,7 +79,7 @@ def test_infant_mort():
         100, 0, 100, graph=False)
     # check that infant mortality equals rate hardcoded into
     # demographics.py
-    assert (infmort_rate == 0.00587)
+    assert (infmort_rate == 0.00566)
 
 
 def test_pop_rebin():

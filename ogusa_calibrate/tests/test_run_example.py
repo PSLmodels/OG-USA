9cb6e2ab820008252badc8479e31f33c5583330d
jdebacker <jason.debacker@gmail.com>
2021-06-21 11:25:39 -0400

    format

---
+++ ogusa_calibrate/tests/test_run_example.py
@@ -0,0 +1,42 @@
+"""
+This model tests whether using the `OG-UK/examples/run_og_uk.py`
+work by making sure that it does not break (is still running) after
+5 minutes (300 seconds).
+"""
+import multiprocessing
+import time
+import os
+import sys
+import importlib.util
+from pathlib import Path
+import pytest
+
+
+def call_run_og_uk():
+    cur_path = os.path.split(os.path.abspath(__file__))[0]
+    path = Path(cur_path)
+    roe_fldr = os.path.join(path.parent.parent, "examples")
+    roe_file_path = os.path.join(roe_fldr, "run_og_uk.py")
+    spec = importlib.util.spec_from_file_location(
+        "run_og_uk.py", roe_file_path
+    )
+    roe_module = importlib.util.module_from_spec(spec)
+    sys.modules["run_og_uk.py"] = roe_module
+    spec.loader.exec_module(roe_module)
+    roe_module.main()
+
+
+def test_run_og_uk(f=call_run_og_uk):
+    p = multiprocessing.Process(target=f, name="run_og_uk", args=())
+    p.start()
+    time.sleep(300)
+    if p.is_alive():
+        p.terminate()
+        p.join()
+        timetest = True
+    else:
+        print("run_og_uk.py did not run for minimum time")
+        timetest = False
+    print("timetest ==", timetest)
+
+    assert timetest

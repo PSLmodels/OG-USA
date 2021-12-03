258e1c0759856ded5d382e4c63eac2079d9e6a15
jdebacker <jason.debacker@gmail.com>
2021-12-02 22:28:22 -0500

    update psid data setup tests

--- ogusa/tests/test_psid_data_setup.py
+++ ogusa/tests/test_psid_data_setup.py
@@ -1,6 +1,50 @@
-import ogusa
+import os
+import numpy as np
+import math
 from ogusa import psid_data_setup
+from ogusa.constants import PSID_NOMINAL_VARS, PSID_CONSTANT_VARS
+
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def test_prep_data():
-    psid_data_setup.prep_data()
+    """
+    Test that function works
+    """
+    _ = psid_data_setup.prep_data()
+
+
+def test_num_obs_total():
+    """
+    Check that number of obs in final data equals what in psid
+    after sample selection
+    """
+    panel_li = psid_data_setup.prep_data()
+    num_obs_psid = 109155
+    if panel_li.shape[0] != num_obs_psid:
+        print("Number of observations in final data set is not right")
+        print("Obs in PSID after selection = ", num_obs_psid)
+        print("Obs in final panel = ", panel_li.shape[0])
+        assert False
+
+
+def test_indicator_vars():
+    """
+    Check that every observation has a group and decile and that
+    fraction in each is correct. Note that can't check the latter with
+    final unbalanced panel.
+    """
+    panel_li = psid_data_setup.prep_data()
+    groups = [0.0, 0.25, 0.5, 0.7, 0.8, 0.9, 0.99, 1.0]
+    cats_pct = ["0-25", "26-50", "51-70", "71-80", "81-90", "91-99", "100"]
+    cats_10 = ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"]
+    print("Checking counts of percentile groupings: ")
+    for item in cats_10 + cats_pct:
+        assert panel_li[item].count() == panel_li.shape[0]
+    # NOTE: the following doesn't work with the unbalanced panel
+    # print("Checking percentile groupings: ")
+    # for d in cats_10:
+    #     assert math.isclose(panel_li[d].mean(), 0.1, rel_tol=0.03)
+    # for i, g in enumerate(cats_pct):
+    #     percent_in_g = groups[i + 1] - groups[i]
+    #     assert math.isclose(panel_li[g].mean(), percent_in_g, rel_tol=0.03)

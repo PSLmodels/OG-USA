c6813064ec18c55a5a3fe233b7cfda82db1d1538
jdebacker <jason.debacker@gmail.com>
2022-07-21 08:58:28 -0400

    black

--- ogusa/calibrate.py
+++ ogusa/calibrate.py
@@ -113,7 +113,9 @@ class Calibration:
         # If run_micro is false, check to see if parameters file exists
         # and if it is consistent with Specifications instance
         if not run_micro:
-            dict_params, run_micro = self.read_tax_func_estimate(p, tax_func_path)
+            dict_params, run_micro = self.read_tax_func_estimate(
+                p, tax_func_path
+            )
         if run_micro:
             micro_data, _ = get_micro_data.get_data(
                 baseline=p.baseline,

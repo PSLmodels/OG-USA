236719509240994f06ffc4866a793b6407c62d24
jdebacker <jason.debacker@gmail.com>
2023-08-14 12:40:19 -0400

    black format

--- ogusa/calibrate.py
+++ ogusa/calibrate.py
@@ -61,7 +61,9 @@ class Calibration:
             p.E, p.S, p.T, 1, 100, p.start_year - 1, p.start_year
         )
         # demographics for 80 period lives (needed for getting e below)
-        demog80 = demographics.get_pop_objs(20, 80, p.T, 1, 100, p.start_year -1, p.start_year)
+        demog80 = demographics.get_pop_objs(
+            20, 80, p.T, 1, 100, p.start_year - 1, p.start_year
+        )
 
         # earnings profiles
         self.e = income.get_e_interp(

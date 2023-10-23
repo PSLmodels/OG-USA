05296e6ada92794a35543c7238c4e715af533a88
Richard Evans <rickecon@gmail.com>
2023-10-23 12:53:56 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ogusa",
-    version="0.1.0",
+    version="0.1.1",
     author="Jason DeBacker and Richard W. Evans",
     license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
     description="USA calibration for OG-Core",

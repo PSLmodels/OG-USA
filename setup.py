2f944686a668fa3ba4b4020cc62c701ac2b4c178
Richard Evans <rickecon@gmail.com>
2023-10-23 18:07:58 -0600

    Updated publish_to_pypi.yml

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
-import setuptools
+"""This file contains the OG-USA package's metadata and dependencies."""
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    longdesc = fh.read()
+from setuptools import find_packages, setup
 
-setuptools.setup(
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
     name="ogusa",
     version="0.1.1",
     author="Jason DeBacker and Richard W. Evans",
     license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
     description="USA calibration for OG-Core",
+    long_description=readme,
     long_description_content_type="text/markdown",
-    long_description=longdesc,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: CC0 1.0 Universal public domain dedication",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+    keywords="USA calibration of large scale overlapping generations model of fiscal policy",
     url="https://github.com/PSLmodels/OG-USA/",
     download_url="https://github.com/PSLmodels/OG-USA/",
     project_urls={
@@ -19,7 +34,7 @@ setuptools.setup(
     packages=["ogusa"],
     package_data={"ogusa": ["ogusa_default_parameters.json", "data/PSID/*"]},
     include_packages=True,
-    python_requires=">=3.7.7, <3.11",
+    python_requires=">=3.7.7, <3.12",
     install_requires=[
         "numpy",
         "psutil",
@@ -37,21 +52,8 @@ setuptools.setup(
         "openpyxl>=3.1.2",
         "statsmodels",
         "linearmodels",
+        "wheel",
         "ogcore",
-    ],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: CC0 1.0 Universal public domain dedication",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     tests_require=["pytest"],
 )

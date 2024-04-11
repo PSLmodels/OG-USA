0d5944f6e01d5a8f3e9a871abedc9fdd2f94fc3b
Richard Evans <rickecon@gmail.com>
2024-04-11 16:18:16 -0600

    Updated test_get_data() and corresponding data in test_get_micro_data.py

--- tests/test_get_micro_data.py
+++ tests/test_get_micro_data.py
@@ -213,7 +213,10 @@ def test_get_data(baseline, dask_client):
         client=dask_client,
         num_workers=NUM_WORKERS,
     )
-    for k, v in test_data.items():
+    # remove last two years from test_data, because get_data() method gives 4 years
+    keys = ['2031', '2032']
+    test_data2 = {x:test_data[x] for x in keys}
+    for k, v in test_data2.items():
         try:
             assert_frame_equal(expected_data[k], v)
         except KeyError:

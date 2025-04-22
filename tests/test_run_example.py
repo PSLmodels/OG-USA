4aac67f2947101a03adf1c86bafe0ec79548921a
Richard Evans <rickecon@gmail.com>
2025-04-21 23:30:45 -0600

    Updated test_run_example.py

--- tests/test_run_example.py
+++ tests/test_run_example.py
@@ -1,5 +1,5 @@
 """
-This model tests whether using the `OG-USA/examples/run_og_usa.py`
+This model tests whether using the `OG-USA/examples/run_ogusa.py`
 work by making sure that it does not break (is still running) after
 5 minutes (300 seconds).
 """
@@ -13,23 +13,23 @@ from pathlib import Path
 import pytest
 
 
-def call_run_og_usa():
+def call_run_ogusa():
     cur_path = os.path.split(os.path.abspath(__file__))[0]
     path = Path(cur_path)
     roe_fldr = os.path.join(path, "..", "examples")
-    roe_file_path = os.path.join(roe_fldr, "run_og_usa.py")
+    roe_file_path = os.path.join(roe_fldr, "run_ogusa.py")
     spec = importlib.util.spec_from_file_location(
-        "run_og_usa.py", roe_file_path
+        "run_ogusa.py", roe_file_path
     )
     roe_module = importlib.util.module_from_spec(spec)
-    sys.modules["run_og_usa.py"] = roe_module
+    sys.modules["run_ogusa.py"] = roe_module
     spec.loader.exec_module(roe_module)
     roe_module.main()
 
 
 @pytest.mark.local
-def test_run_og_usa(f=call_run_og_usa):
-    p = multiprocessing.Process(target=f, name="run_og_usa", args=())
+def test_run_ogusa(f=call_run_ogusa):
+    p = multiprocessing.Process(target=f, name="run_ogusa", args=())
     p.start()
     time.sleep(300)
     if p.is_alive():
@@ -37,7 +37,7 @@ def test_run_og_usa(f=call_run_og_usa):
         p.join()
         timetest = True
     else:
-        print("run_og_usa.py did not run for minimum time")
+        print("run_ogusa.py did not run for minimum time")
         timetest = False
     print("timetest ==", timetest)
 

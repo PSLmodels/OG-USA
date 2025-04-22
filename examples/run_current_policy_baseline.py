9f189caa2ec67b46862a4b4d4c35db65d4a9dd3c
Richard Evans <rickecon@gmail.com>
2025-04-22 01:24:15 -0600

    Fixed table command at end of run_current_policy_baseline.py

--- examples/run_current_policy_baseline.py
+++ examples/run_current_policy_baseline.py
@@ -185,7 +185,7 @@ def main():
         os.path.join(save_dir, "cp_example_plots_tables"),
     )
     # Create CSV file with output
-    ot.tp_output_dump_table(
+    ot.time_series_table(
         base_params,
         base_tpi,
         reform_params,
